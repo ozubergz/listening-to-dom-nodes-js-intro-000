3d2ffe0a2eb4d2f564fc48a1f97a94c10849040b
James Min <jmin499@gmail.com>
2020-10-02 03:32:38 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -21,20 +21,10 @@ input.addEventListener('keydown', function(e) {
 let divs = document.querySelectorAll('div');
 
 function bubble(e) {
+  e.stopPropagation()
   console.log(this.firstChild.nodeValue.trim() + ' bubbled');
 }
 
 for (let i = 0; i < divs.length; i++) {
   divs[i].addEventListener('click', bubble);
-}
-
-divs = document.querySelectorAll('div');
-
-function capture(e) {
-  console.log(this.firstChild.nodeValue.trim() + ' captured');
-}
-
-for (let i = 0; i < divs.length; i++) {
-  // set the third argument to `true`!
-  divs[i].addEventListener('click', capture, true);
 }
