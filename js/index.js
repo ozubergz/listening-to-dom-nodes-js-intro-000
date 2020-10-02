2f2bd0dcf3e06b1c512b204ffd68d4a5df116233
James Min <jmin499@gmail.com>
2020-10-02 03:18:31 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -5,3 +5,14 @@ const main = document.getElementById('main');
 main.addEventListener('click', function(event) {
   alert('I was clicked!');
 });
+
+
+const input = document.querySelector('input')
+
+input.addEventListener('keydown', function(e) {
+  if (e.key === "g") {
+    return e.preventDefault()
+  } else {
+    console.log(e.key)
+  }
+});
